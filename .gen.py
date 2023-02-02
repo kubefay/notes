1adb51f9e8e1a749e1f0f913ac7e4eebd1a0913c
kubefay <wxjpython@163.com>
2023-02-02 14:49:49 +0800

    a

--- .gen.py
+++ .gen.py
@@ -19,9 +19,8 @@ for root, dirs, files in os.walk("."):
     for name in files:
         file_name = os.path.basename(name)
         file_name = file_name.split('.')[1].strip()
-        name_path = 
         
-
-        result.append(f'[{file_name}]({root[2:]}/{name})\n')
+        # result.append(f'[{file_name}]({root[2:]}/{name})\n')
+        result.append(f'''<a href="{root[2:]}/{name}">{file_name}</a>\n''')
 
 print(''.join(result))
--- AI/deploy/TensorRT/NvidiaBlog/title.md
+++
@@ -1,1 +0,0 @@
-Nvdia TensorRT教程
